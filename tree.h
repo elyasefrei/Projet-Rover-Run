a8049900f424dd4e18d7614168c32b4538f55289
elyasefrei <elyas.tarzet@efrei.net>
2024-11-15 19:21:03 +0100

    Update tree.h

--- tree.h
+++ tree.h
@@ -1,6 +1,7 @@
 #ifndef UNTITLED1_TREE_H
 #define UNTITLED1_TREE_H
-
+#define MVT_TOTAL 100
+#define NBR_MVT 9
     typedef struct s_node
     {
         int value;
@@ -15,5 +16,7 @@ t_node * create_node(int depth, int nbSons, t_node* parent);
 void create_children(t_node *parent, int maxDepth);
 int* mouvement_aleatoire(int n );
 int* create_tableau(int n);
+void dfs(t_node *node, int MAX_DEPTH,t_node**tableau,int* compteur);
+t_node* low_node(t_node **tableau,int taille, int MAX_DEPTH);
 
 #endif //UNTITLED1_TREE_H
