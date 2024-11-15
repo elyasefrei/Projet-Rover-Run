9f8ce0e3e4ae861d41d7ae4da40d1c0fb5e09850
elyasefrei <elyas.tarzet@efrei.net>
2024-11-15 17:46:11 +0100

    Update tree.h

--- tree.h
+++ tree.h
@@ -4,14 +4,16 @@
     typedef struct s_node
     {
         int value;
+        int movement;
         int depth;
-        struct s_node *parent;
         struct s_node **sons;
         int nbSons;
+        struct s_node *parent;
     } t_node;
 
-t_node * create_node(int value, int depth, int nbSons);
-
+t_node * create_node(int depth, int nbSons, t_node* parent);
 void create_children(t_node *parent, int maxDepth);
+int* mouvement_aleatoire(int n );
+int* create_tableau(int n);
 
 #endif //UNTITLED1_TREE_H
