ba84b5f37cf9287ef2f32715176c3fdf40422f27
elyasefrei <elyas.tarzet@efrei.net>
2024-11-24 19:35:23 +0100

    Update tree.c

--- tree.c
+++ tree.c
@@ -3,16 +3,16 @@
 #include "tree.h"
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "tree.h"
-
-
 t_node * create_node(int depth, int nbSons, t_node* parent) {
     t_node *node = malloc(sizeof(t_node));
     node->value = 0;
     node->depth = depth;
-    node->nbSons = nbSons;
+    if(depth==MAX_DEPTH) {
+        node->nbSons=0;
+    }
+    else {
+        node->nbSons=nbSons;
+    }
     node->sons = malloc(nbSons * sizeof(t_node *));
     node->movement=0;
     node->parent=parent;
@@ -23,33 +23,14 @@ t_node * create_node(int depth, int nbSons, t_node* parent) {
 }
 
 
-void create_children(t_node *parent, int maxDepth) {
-    if (parent->depth >= maxDepth) {
+void create_children(t_node *parent) {
+    if (parent->depth >= MAX_DEPTH) {
         return;
     }
 
     for (int i = 0; i < parent->nbSons; i++) {
         parent->sons[i] = create_node(parent->depth + 1, parent->nbSons - 1,parent);
-        create_children(parent->sons[i], maxDepth);
+        create_children(parent->sons[i]);
     }
 }
-
-
-/*
-t_node* ResearchVal(t_node *node, int MAX_DEPTH, int min_cost,t_node *Bestnode) {
-    if (node->depth != MAX_DEPTH) {
-        for(int i=0;i<node->nbSons;i++) {
-            ResearchVal(node->sons[i], MAX_DEPTH, min_cost,Bestnode);
-        }
-    }
-    else {
-        if(node->value<min_cost){
-            min_cost=node->value;
-            Bestnode=node;
-            return Bestnode;
-
-        }
-    }
-}
-*/
 
