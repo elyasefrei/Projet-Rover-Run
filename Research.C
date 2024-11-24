ebb0f791055406d9a38df660c1fd5663e65ccec2
elyasefrei <elyas.tarzet@efrei.net>
2024-11-24 15:14:35 +0100

    Update Research.C

--- Research.C
+++ Research.C
@@ -3,21 +3,21 @@
 #include "map.h"
 
 //allocation de la mémoire de tableau dans le main//
-void dfs(t_node *node, int MAX_DEPTH,t_node**tableau,int* compteur)
+void dfs(t_node *node,t_node**tableau,int*   compteur)
 {
 
     tableau[*compteur]=node;
-    compteur++;
+    (*compteur)++;
     if(node->depth<MAX_DEPTH && node->value<=500) {
         for (int i=0;i<node->nbSons;i++) {
-            dfs(node->sons[i],MAX_DEPTH,tableau,compteur);
+            dfs(node->sons[i],tableau,compteur);
         }
     }
 
 }
 
-//taille est le compteur de la fonction d'au dessus//
-t_node* low_node(t_node **tableau,int taille, int MAX_DEPTH) {
+//taille c'est le compteur de la fonction d'au dessus//
+t_node* low_node(t_node **tableau,int taille) {
     int  min_cost=COST_UNDEF;
     t_node* pbestnode=NULL;
     for (int i=0;i<taille;i++) {
@@ -33,8 +33,7 @@ t_node* low_node(t_node **tableau,int taille, int MAX_DEPTH) {
 
 
 //Recursif mais ne marche pas//
-/*
-t_node* ResearchVal(t_node *node, int MAX_DEPTH, int min_cost,t_node *Bestnode) {
+/*t_node* ResearchVal(t_node *node, int MAX_DEPTH, int min_cost,t_node *Bestnode) {
     if (node->depth != MAX_DEPTH) {
         for(int i=0;i<node->nbSons;i++) {
             ResearchVal(node->sons[i], MAX_DEPTH, min_cost,Bestnode);
@@ -49,3 +48,24 @@ t_node* ResearchVal(t_node *node, int MAX_DEPTH, int min_cost,t_node *Bestnode) 
         }
     }
 }*/
+
+
+
+int produit(int nbMouvements, int d) {
+    int resultat = 1;
+    for (int k = 0; k < d; k++) {
+        resultat *= (nbMouvements - k);
+    }
+    return resultat;
+}
+
+
+int calculer_total_noeuds(int nbMouvements) {
+    int total = 0;
+
+    for (int d = 0; d <= MAX_DEPTH; d++) {
+        total += produit(nbMouvements, d);
+    }
+
+    return total;
+}
